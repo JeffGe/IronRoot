1e11594da5573a5042dbd6f756f62a3c7ec1a0e6
jeffge <jeffge926@gmail.com>
2016-05-03 18:33:49 -0500

    Start working on dashbaord page

---
+++ src/dashboard/dashboard.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<style>
+
+    .dashboardPage{
+        height: 100px;
+        width: 100px;
+        background: blue;
+        
+    }
+
+</style>
+<html>
+<body>
+
+<div class="sidebar">
+    <?php include '../../src/sideBar/sideBar.php';?>
+
+    <div class="dashboardPage">
+        fasdf
+        asdfasdfasdf
+        asdf
+    </div>
+
+</div>
+
+
+
+</body>
+</html>
